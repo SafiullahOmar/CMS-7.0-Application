8b942b68e6c224b40e8f12914fafd2de7bb25039
Safiullah Omar <omar.safiullah@gmail.com>
2023-03-22 15:21:38 +0430

    Registraion post method

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication1.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,36 +14,44 @@ namespace WebApplication1.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
-        // GET: api/<AccountController>
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signManager;
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
-            return new string[] { "value1", "value2" };
+            _userManager = userManager;
+            _signManager = signInManager;
         }
 
-        // GET api/<AccountController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+
+        [HttpPost("action")]
+        public async Task<ActionResult> Register([FromBody] RegistrationViewModel formdata)
         {
-            return "value";
+            List<string> Errorlist = new List<string>();
+            var user = new IdentityUser
+            {
+                Email = formdata.Email,
+                UserName = formdata.UserName,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+
+            var result = await _userManager.CreateAsync(user, formdata.Password);
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, "Customer");
+                return Ok(new { username = user.UserName, email = user.Email, status = 1, message = "Registration Successfull" });
+            }
+            else {
+
+                foreach (var error in result.Errors) {
+                    ModelState.AddModelError("", error.Description);
+                    Errorlist.Add(error.Description);
+                }
+            }
+
+            return BadRequest(new JsonResult(Errorlist));
+
         }
 
-        // POST api/<AccountController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
 
-        // PUT api/<AccountController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<AccountController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
